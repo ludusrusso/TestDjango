0de559e4a0c2df748a55577bee7a8fc025fe9063
Ludovico Orlando Russo <ludus.russo@gmail.com>
2017-06-18 20:11:38 +0200

    first FT in comments and now dev in unittest

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,29 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://127.0.0.1:8000')
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
 
-assert 'Django' in browser.title
+    def tearDown(self):
+        self.browser.quit()
+        self.browser.implicit_wayt(3)
+
+
+    def test_can_browse_the_blog(self):
+        # Ludus is looking for the brand new blog ludusblog
+        # he opens Firefox and browses to the link
+        self.browser.get('http://127.0.0.1:8000')
+
+        # Ludus notice that the title of the page is LudusBlog
+        self.assertIn('LudusBlog', self.browser.title)
+        self.fail('finish the test')
+
+        # Ludus notice that there is an interesting post called 'first post'
+
+        # Ludus clicks on the post and start reading, he notice
+
+        # Ludus is satisfed and closes the blog.
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
